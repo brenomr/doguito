fbd19e82ea879f4833275056472d2fcbe9e5d897
Breno Rodrigues <brenocartoon@hotmail.com>
2021-12-15 21:58:47 -0300

    Adding new pages to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './assets/css/base/base.css'
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/sobre'>
+            <Sobre />
+          </Route>
+          <Route>
+            <Pagina404 />
+          </Route>
+        </Switch>
+      </Router>
     </>
   )
 }
